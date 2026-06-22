e0ed505ba5ba93e8d9608b173ef0ecef63fc5de0
Niidhii-003 <nidhivermalko90@gmail.com>
2026-06-22 15:28:21 +0530

    Updates binarytree.java

--- binarytree.java
+++ binarytree.java
@@ -21,11 +21,22 @@ public class binarytree{
             newNode.right=buildTree(nodes);
             return newNode; 
         }
+        ///preorder
+        public static void preorder(Node root){
+            if(root==null){
+                return;
+            }
+            System.out.println(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+
+        }
     }
+    
     public static void main(String args[]){
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data); 
+        tree.preorder(root);
     }
 }
