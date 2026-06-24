1c6e50ddc13aeea5cfcfb6be3acdf0a7ad51a871
Niidhii-003 <nidhivermalko90@gmail.com>
2026-06-25 00:22:31 +0530

    changes

--- treepart2.java
+++ treepart2.java
@@ -85,6 +85,81 @@ import java.util.*;
 
 // SUBTREE
 
+// public class treepart2{
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
+        
+//         public Node(int data){
+//             this.data = data;
+//             this.left=null;
+//             this.right=null;
+//         }
+//     }
+//     public static boolean isIdentical(Node node, Node subRoot){
+//         if(node==null && subRoot==null){
+//             return true;
+//         }
+//         else if(node==null || subRoot==null || node.data!=subRoot.data){
+//             return false;
+//         }
+//         if(!isIdentical(node.left, subRoot.left)){
+//             return false;
+//         }
+//         if(!isIdentical(node.right, subRoot.right)){
+//             return false;
+//         }
+//         return true;
+//     }
+
+
+//     public static boolean isSubtree(Node root, Node subRoot){
+//         if(root == null){
+//             return false;
+//         }
+//         if(root.data == subRoot.data){
+//             if(isIdentical(root, subRoot)){
+//                 return true;
+//             }
+//         }
+//         return  isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     }
+//     public static void main(String args[]){
+//         /*  
+//              1
+//             /  \
+//            2    3
+//           / \   / \
+//          4   5  6  7
+
+//          */
+
+//         Node root = new Node(1);
+//         root.left = new Node(2);
+//         root.right = new Node(3);
+//         root.left.left = new Node(4);
+//         root.left.right = new Node(5);
+//         root.right.left = new Node(6);
+//         root.right.right = new Node(7);
+
+//             //   2 
+//             //  /  \
+//             // 4    5
+        
+//         Node subRoot = new Node(2);
+//         subRoot.left = new Node(4);
+//         subRoot.right = new Node(5);
+//         System.out.println(isSubtree(root, subRoot));
+//     }
+// }
+
+
+//--------------------------------------------------------
+
+//TOP VIEW
+import java.util.*;
+
 public class treepart2{
     static class Node{
         int data;
@@ -97,33 +172,51 @@ public class treepart2{
             this.right=null;
         }
     }
-    public static boolean isIdentical(Node node, Node subRoot){
-        if(node==null && subRoot==null){
-            return true;
+    static class Info{
+        Node node;
+        int hd;
+
+        public Info(Node node, int hd){
+            this.node = node;
+            this.hd = hd;
         }
-        else if(node==null || subRoot==null || node.data!=subRoot.data){
-            return false;
-        }
-        if(!isIdentical(node.left, subRoot.left)){
-            return false;
-        }
-        if(!isIdentical(node.right, subRoot.right)){
-            return false;
-        }
-        return true;
     }
 
-
-    public static boolean isSubtree(Node root, Node subRoot){
-        if(root == null){
-            return false;
-        }
-        if(root.data == subRoot.data){
-            if(isIdentical(root, subRoot)){
-                return true;
+    public static void topView(Node root){
+        //level order
+        Queue<Info> q = new java.util.LinkedList<Info>();
+        HashMap<Integer, Node> map = new HashMap<>();
+        int min=0, max=0;
+        q.add(new Info(root, 0));  ///root hd =0
+        q.add(null);
+        while(!q.isEmpty()){
+            Info curr=q.remove();
+            if(curr==null){
+                if(q.isEmpty()){
+                    break;
+                }else{
+                    q.add(null);
+                }
+            }else{
+                if(!map.containsKey(curr.hd)){ //1st time hd occur
+                    map.put(curr.hd, curr.node);
+                }
+                if(curr.node.left!=null){
+                    q.add(new Info(curr.node.left, curr.hd-1));
+                    min=Math.min(min, curr.hd-1);
+                }
+                if(curr.node.right!=null){
+                    q.add(new Info(curr.node.right, curr.hd+1));
+                    max=Math.max(max, curr.hd+1);
+               }    
             }
         }
-        return  isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        for(int i=min; i<=max; i++){
+            if(map.containsKey(i)){
+                System.out.print(map.get(i).data+" ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String args[]){
         /*  
@@ -142,14 +235,7 @@ public class treepart2{
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
+        topView(root);
 
-            //   2 
-            //  /  \
-            // 4    5
-        
-        Node subRoot = new Node(2);
-        subRoot.left = new Node(4);
-        subRoot.right = new Node(5);
-        System.out.println(isSubtree(root, subRoot));
     }
 }
